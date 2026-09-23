d81c99768056360fa63bde3aeae40b70ffc9c9af
amor <50753427+amordz72@users.noreply.github.com>
2026-09-23 07:33:57 +0100

    Refresh editable autofill routes

--- smart-intake.js
+++ smart-intake.js
@@ -52,7 +52,7 @@
       source_kind:'paste',
       origin:'mytool_home'
     }));
-    location.href='orders/?intake=1&v=20260922-smart-intake1';
+    location.href='orders/?intake=1&v=20260923-editable-autofill1';
   }
 
   function mount(){
